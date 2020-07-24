de89475a013bfab881a77c67da6fb99be486dfe2
developerbch <designer.bch@gmail.com>
2020-07-24 16:29:31 +0900

    #3.18 isLiked Computed File

--- src/api/User/computed.js
+++ src/api/User/computed.js
@@ -31,4 +31,24 @@ export default {
       return user.id === parentId;
     },
   },
+  Post: {
+    isLiked: (parent, _, { request }) => {
+      const { user } = request;
+      const { id } = parent;
+      return prisma.$exists.like({
+        AND: [
+          {
+            user: {
+              id: user.id,
+            },
+          },
+          {
+            post: {
+              id,
+            },
+          },
+        ],
+      });
+    },
+  },
 };
