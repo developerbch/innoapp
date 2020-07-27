5c24a9bb16dadc375b0957992de78474d7ecefdc
developerbch <designer.bch@gmail.com>
2020-07-27 12:17:49 +0900

    #3.20 seeFullPost Refactoring

--- src/api/User/computed.js
+++ src/api/User/computed.js
@@ -31,24 +31,4 @@ export default {
       return user.id === parentId;
     },
   },
-  Post: {
-    isLiked: (parent, _, { request }) => {
-      const { user } = request;
-      const { id } = parent;
-      return prisma.$exists.like({
-        AND: [
-          {
-            user: {
-              id: user.id,
-            },
-          },
-          {
-            post: {
-              id,
-            },
-          },
-        ],
-      });
-    },
-  },
 };
--- src/fragments.js
+++ src/fragments.js
@@ -1,9 +1,35 @@
+// UserParts에 집중하지말고 User에만 집중하면된다. User는 datamodel에 있는거하고 같은 이름으로 해줘야한다.
+export const USER_FRAGMENT = `
+    id
+    userName
+`;
+
 export const COMMENT_FRAGMENT = `
-    fragment CommentParts on Comment{
+    id
+    text
+    user {
+        ${USER_FRAGMENT}
+    }
+`;
+
+export const FILE_FRAGMENT = `
+    id
+    url
+`;
+
+export const FULL_POST_FRAGMENT = `
+    fragment PostParts on Post{
         id
-        text
-        user {
-            userName
+        location
+        caption
+        files{
+            ${FILE_FRAGMENT}
+        }
+        comments{
+            ${COMMENT_FRAGMENT}
+        }
+        user{
+            ${USER_FRAGMENT}
         }
     }
 `;
