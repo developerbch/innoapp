c553b3390754026d9dca592c68776beba8682e5e
developerbch <designer.bch@gmail.com>
2020-07-28 18:20:48 +0900

    Minifixes

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -1,15 +1,16 @@
 import { prisma } from "../../../../generated/prisma-client";
+
 export default {
   Mutation: {
     createAccount: async (_, args) => {
-      const { userName, email, firstName = "", lastName = "" } = args;
-      const user = await prisma.createUser({
+      const { userName, email, firstName = "", lastName = "", bio = "" } = args;
+      await prisma.createUser({
         userName,
         email,
         firstName,
         lastName,
+        bio,
       });
-      return user;
     },
   },
 };
