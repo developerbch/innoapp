aa8bfb35bb2ca0c6d68057ef5a13a3e587533efb
developerbch <designer.bch@gmail.com>
2020-09-07 15:29:33 +0900

    add upload file(image) to AWS (but, delete soon)

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import schema from "./schema";
 import "./passport";
 import { authenticateJwt } from "./passport";
 import { isAuthenticated } from "./middlewares";
+import { uploadController, uploadMiddleware } from "./upload";
 
 const PORT = process.env.PORT || 4000;
 
@@ -15,7 +16,7 @@ const server = new GraphQLServer({
 
 server.express.use(logger("dev")); //express 서버에서 logger 미들웨어를 사용. 사실은 morgan 모듈이지! dev라는 옵션을 줌
 server.express.use(authenticateJwt);
-
+server.express.post("/api/upload", uploadMiddleware, uploadController);
 server.start({ port: PORT }, () =>
   console.log(`Server running on  http://localhost:${PORT}`)
 );
